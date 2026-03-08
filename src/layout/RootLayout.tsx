7c0b2f04c0cfa6fcf4b6968dd39aa2821a06a683
Raturchik <artyrkafarov@gmail.com>
2026-03-08 23:52:01 +0300

    feat: implement new components in RootLayout

--- src/layout/RootLayout.tsx
+++ src/layout/RootLayout.tsx
@@ -1,9 +1,13 @@
 import { Outlet } from "react-router";
+import { Footer } from "../components/Footer";
+import { Header } from "../components/Header";
 
 export const RootLayout = () => {
     return (
         <div>
+            <Header />
             <Outlet />
+            <Footer />
         </div>
     );
 };
