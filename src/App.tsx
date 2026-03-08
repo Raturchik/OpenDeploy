da608cd918d2d606ef1e08e690e1d9df97278703
Raturchik <artyrkafarov@gmail.com>
2026-03-08 23:38:06 +0300

    feat: create basic routing structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import {
+    createBrowserRouter,
+    createRoutesFromElements,
+    Route,
+    RouterProvider,
+} from "react-router";
+import { RootLayout } from "./layout/RootLayout";
+
 function App() {
-    return <></>;
+    const routes = createRoutesFromElements(
+        <Route element={<RootLayout />} path="/"></Route>,
+    );
+    const router = createBrowserRouter(routes);
+    return <RouterProvider router={router} />;
 }
 
 export default App;
