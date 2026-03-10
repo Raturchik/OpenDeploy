249030d80537a854fcd4efeee4fcfcba7a95ad23
Артур <artyrkafarov@gmail.com>
2026-03-10 21:23:20 +0300

    Feature/project_routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,24 @@
-function App() {
-    return <></>;
-}
+import {
+    createBrowserRouter,
+    createRoutesFromElements,
+    Route,
+    RouterProvider,
+} from "react-router";
+import { RootLayout } from "./layout/RootLayout";
+import { HomePage } from "./pages/HomePage";
+import { AuthorisationPage } from "./pages/AuthorisationPage";
+import { AddBuild } from "./pages/AddBuild";
+
+const App = () => {
+    const routes = createRoutesFromElements(
+        <Route element={<RootLayout />} path="/">
+            <Route element={<HomePage />} index />
+            <Route element={<AuthorisationPage />} path="auth" />
+            <Route element={<AddBuild />} path="add" />
+        </Route>,
+    );
+    const router = createBrowserRouter(routes);
+    return <RouterProvider router={router} />;
+};
 
 export default App;
