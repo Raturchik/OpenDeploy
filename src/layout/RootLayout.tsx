2d08ab546e9bd2c0b414d159276cebaf2c6b5251
Артур <artyrkafarov@gmail.com>
2026-03-10 22:00:58 +0300

    refactor: create index.ts files and change import (#4)

--- src/layout/RootLayout.tsx
+++ src/layout/RootLayout.tsx
@@ -1,6 +1,5 @@
 import { Outlet } from "react-router";
-import { Footer } from "../components/Footer";
-import { Header } from "../components/Header";
+import { Header, Footer } from "../components";
 
 export const RootLayout = () => {
     return (
