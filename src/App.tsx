c8312275d16caccedd3ebe3216813b722d4837f0
Raturchik <artyrkafarov@gmail.com>
2026-03-10 20:56:33 +0300

    feat: implement pages in App routing

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,17 @@ import {
     RouterProvider,
 } from "react-router";
 import { RootLayout } from "./layout/RootLayout";
+import { HomePage } from "./pages/HomePage";
+import { AuthorisationPage } from "./pages/AuthorisationPage";
+import { AddBuild } from "./pages/AddBuild";
 
 function App() {
     const routes = createRoutesFromElements(
-        <Route element={<RootLayout />} path="/"></Route>,
+        <Route element={<RootLayout />} path="/">
+            <Route element={<HomePage />} index />
+            <Route element={<AuthorisationPage />} path="auth" />
+            <Route element={<AddBuild />} path="add" />
+        </Route>,
     );
     const router = createBrowserRouter(routes);
     return <RouterProvider router={router} />;
