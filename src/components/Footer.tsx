3c5ec332298340d424fcaee90d1f430b2d52f742
Raturchik <artyrkafarov@gmail.com>
2026-03-14 17:16:03 +0300

    feat: create Footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,20 @@
 export const Footer = () => {
-    return <div>Footer</div>;
+    return (
+        <footer className="text-center m-7">
+            <p>
+                Created By 😛 <a href="">Raturchik(LinkedIn)</a> © 2026 &nbsp;
+                <a
+                    href="https://github.com/Raturchik/OpenDeploy/tree/master"
+                    target="_blank"
+                >
+                    <span className=" font-black tracking-tight text-foreground">
+                        Open
+                        <span className="text-[rgb(13,161,230)] sans-serif">
+                            Deploy
+                        </span>
+                    </span>
+                </a>
+            </p>
+        </footer>
+    );
 };
