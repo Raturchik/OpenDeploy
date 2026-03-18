1e789d95ff805c86884b6faf58fd3c54f9b50006
Raturchik <artyrkafarov@gmail.com>
2026-03-19 00:01:14 +0300

    feat: add cards hover animation

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -3,19 +3,14 @@ import { IoMdCode } from "react-icons/io";
 
 export const Card = () => {
     return (
-        <div className="p-3.75 bg-white rounded-3xl ">
+        <div className="p-3.75 bg-white rounded-3xl shadow-sm transform transition-all duration-400 ease-in-out hover:-translate-y-1 hover:scale-[1.01] hover:shadow-lg">
             <h3 className="text-lg font-extrabold text-foreground">Name</h3>
             <p className="mt-1 line-clamp-2 text-sm text-muted-foreground text-gray-400">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                Assumenda, nostrum
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda, nostrum
             </p>
             <div className="mt-3 flex flex-wrap gap-1.5">
-                <span className="rounded-lg px-2.5 py-1 text-xs font-bold ">
-                    react.js
-                </span>
-                <span className=" rounded-lg px-2.5 py-1 text-xs font-bold ">
-                    Node.js
-                </span>
+                <span className="rounded-lg px-2.5 py-1 text-xs font-bold ">react.js</span>
+                <span className=" rounded-lg px-2.5 py-1 text-xs font-bold ">Node.js</span>
             </div>
             <div className="flex justify-between items-center">
                 <a href="" className="flex gap-3">
