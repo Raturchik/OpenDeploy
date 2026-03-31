d41496dd73c127a844163e7023fe2b716a43a46d
Raturchik <artyrkafarov@gmail.com>
2026-03-31 22:47:23 +0300

    feat: create firebase.ts file

---
+++ src/services/firebase/firebase.ts
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCx9nWUZsTxcoZ8VksiRFT-Eht_tRci_xc",
+    authDomain: "opendeploy-fb6be.firebaseapp.com",
+    projectId: "opendeploy-fb6be",
+    storageBucket: "opendeploy-fb6be.firebasestorage.app",
+    messagingSenderId: "280488527741",
+    appId: "1:280488527741:web:6931a0d4f3443caf826e14",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
