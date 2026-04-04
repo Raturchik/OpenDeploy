e6997766939023e339bc62f63d8d6b3ec8b0b82f
Raturchik <artyrkafarov@gmail.com>
2026-04-05 00:07:28 +0300

    feat recreate authorisation by email and password with react hook form

--- src/pages/Authorisation.tsx
+++ src/pages/Authorisation.tsx
@@ -5,47 +5,67 @@ import { useState } from "react";
 import { twMerge } from "tailwind-merge";
 import { auth } from "../services/firebase/firebase";
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { useForm, type SubmitHandler } from "react-hook-form";
+import { useNavigate } from "react-router";
+
+type FormData = {
+    email: string;
+    password: string;
+    username: string;
+    copyPassword: string;
+};
 
 export const AuthorisationPage = () => {
     const [isLogin, setIsLogin] = useState(true);
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [copyPassword, setCopyPassword] = useState("");
     const [error, setError] = useState("");
-    const [username, setUsername] = useState("");
 
-    function signUp(e: React.SubmitEvent<HTMLFormElement>) {
-        e.preventDefault();
-        if (copyPassword !== password) {
-            setError("Passwords didn't match");
+    const navigate = useNavigate();
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+        reset,
+    } = useForm<FormData>();
+
+    function signUp(userData: FormData): void {
+        if (userData.password !== userData.copyPassword) {
+            setError("Your passwords do not match.");
             return;
         }
-        createUserWithEmailAndPassword(auth, email, password)
+        createUserWithEmailAndPassword(auth, userData.email, userData.password)
             .then((user) => {
                 console.log(user);
                 setError("");
-                setEmail("");
-                setPassword("");
+                reset();
+                navigate("/");
             })
             .catch((error) => {
                 console.log(error);
                 setError("Unexpected error ocured");
             });
     }
-    function signIn(e: React.SubmitEvent<HTMLFormElement>) {
-        e.preventDefault();
-        signInWithEmailAndPassword(auth, email, password)
+    function signIn(userData: FormData) {
+        signInWithEmailAndPassword(auth, userData.email, userData.password)
             .then((user) => {
                 console.log(user);
                 setError("");
-                setEmail("");
-                setPassword("");
+                reset();
+                navigate("/");
             })
             .catch((error) => {
                 console.log(error);
                 setError("Unexpected error ocured");
             });
     }
+
+    const onSubmit: SubmitHandler<FormData> = (data) => {
+        if (isLogin) {
+            signIn(data);
+        } else {
+            signUp(data);
+        }
+        console.log(data);
+    };
     return (
         <div className="grid h-screen place-items-center">
             <div className="flex flex-col mx-auto sm:w-[50%] md:w-[45%] lg:w-[40%] xl:w-[35%] 2xl:w-[30%]">
@@ -87,86 +107,102 @@ export const AuthorisationPage = () => {
                         </Button>
                     </div>
                     {isLogin ? (
-                        <form onSubmit={signIn} className="flex flex-col gap-3">
+                        <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-3">
                             <label htmlFor="email">Email</label>
                             <input
                                 type="email"
-                                name=""
                                 id="email"
-                                value={email}
                                 placeholder="you@example.com"
+                                {...register("email", { required: "Email is required" })}
                                 className="outline-none p-3.5 shadow-xl rounded-3xl border border-border/50 border-gray-200 focus:border-2 focus:border-blue-600"
-                                onChange={(e) => setEmail(e.target.value)}
                             />
+                            {errors.email ? (
+                                <div className="text-red-600">{errors.email.message}</div>
+                            ) : (
+                                ""
+                            )}
                             <label htmlFor="password">Password</label>
                             <input
                                 type="password"
-                                name=""
                                 id="password"
-                                value={password}
                                 placeholder="••••••••"
+                                {...register("password", { required: "Password is required" })}
                                 className="outline-none p-3.5 shadow-xl rounded-3xl border border-border/50 border-gray-200 focus:border-2 focus:border-blue-600"
-                                onChange={(e) => setPassword(e.target.value)}
                             />
-
+                            {errors.password ? (
+                                <div className="text-red-600">{errors.password.message}</div>
+                            ) : (
+                                ""
+                            )}
                             <button
                                 type="submit"
                                 className="bg-[rgb(13,161,230)] rounded-3xl p-2.5 cursor-pointer"
-                                onSubmit={() => signIn}
                             >
                                 <p className="text-white text-lg font-bold">Sign In</p>
                             </button>
                         </form>
                     ) : (
-                        <form onSubmit={signUp} className="flex flex-col gap-3">
-                            <label htmlFor="email">Username</label>
+                        <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-3">
+                            <label htmlFor="username">Username</label>
                             <input
                                 type="text"
-                                name=""
                                 id="username"
-                                value={username}
                                 placeholder="your_username"
+                                {...register("username", { required: true })}
                                 className="outline-none p-3.5 shadow-xl rounded-3xl border border-border/50 border-gray-200 focus:border-2 focus:border-blue-600"
-                                onChange={(e) => setUsername(e.target.value)}
                             />
+                            {errors.username ? (
+                                <div className="text-red-600">{errors.username.message}</div>
+                            ) : (
+                                ""
+                            )}
                             <label htmlFor="email">Email</label>
                             <input
                                 type="email"
-                                name=""
                                 id="email"
-                                value={email}
                                 placeholder="you@example.com"
+                                {...register("email", { required: true })}
                                 className="outline-none p-3.5 shadow-xl rounded-3xl border border-border/50 border-gray-200 focus:border-2 focus:border-blue-600"
-                                onChange={(e) => setEmail(e.target.value)}
                             />
+                            {errors.email ? (
+                                <div className="text-red-600">{errors.email.message}</div>
+                            ) : (
+                                ""
+                            )}
                             <label htmlFor="password">Password</label>
                             <input
                                 type="password"
-                                name=""
                                 id="password"
-                                value={password}
                                 placeholder="••••••••"
+                                {...register("password", { required: true })}
                                 className="outline-none p-3.5 shadow-xl rounded-3xl border border-border/50 border-gray-200 focus:border-2 focus:border-blue-600"
-                                onChange={(e) => setPassword(e.target.value)}
                             />
-                            <label htmlFor="password">Re-enter your password</label>
+                            {error ? <div className="text-red-600">{error}</div> : ""}
+                            {errors.password ? (
+                                <div className="text-red-600">{errors.password.message}</div>
+                            ) : (
+                                ""
+                            )}
+                            <label htmlFor="copyPassword">Re-enter your password</label>
                             <input
                                 type="password"
-                                name=""
-                                id="password"
-                                value={copyPassword}
+                                id="copyPassword"
                                 placeholder="••••••••"
+                                {...register("copyPassword", { required: true })}
                                 className="outline-none p-3.5 shadow-xl rounded-3xl border border-border/50 border-gray-200 focus:border-2 focus:border-blue-600"
-                                onChange={(e) => setCopyPassword(e.target.value)}
                             />
-
+                            {error ? <div className="text-red-600">{error}</div> : ""}
+                            {errors.copyPassword ? (
+                                <div className="text-red-600">{errors.copyPassword.message}</div>
+                            ) : (
+                                ""
+                            )}
                             <button
                                 type="submit"
                                 className="bg-[rgb(13,161,230)] rounded-3xl p-2.5 cursor-pointer shadow-2xl"
                             >
                                 <p className="text-white text-lg font-bold">Sign Up</p>
                             </button>
-                            {error ? <p>{error}</p> : ""}
                         </form>
                     )}
                     <p className="text-center text-gray-400">or continue with</p>
